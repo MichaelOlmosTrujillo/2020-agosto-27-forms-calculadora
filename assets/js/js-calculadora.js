4c8a550a10582661b113f36875b4f9476d26097a
Michael Olmos Trujillo <molmost@unal.edu.co>
2020-08-27 17:12:02 -0500

    Calculadora

--- assets/js/js-calculadora.js
+++ assets/js/js-calculadora.js
@@ -1,9 +1,53 @@
+//Promedio
 const promedio = document.getElementById("promedio");
+// Suma
+const suma = document.getElementById("suma");
+const resta = document.getElementById("resta");
+const multiplicacion = document.getElementById("multiplicacion");
+const division = document.getElementById("division");
 /* Llamamos el botón 
  */
 promedio.addEventListener("click", () => {
-    let num1 = document.getElementById("num1"); /* Me trae y almacena el valor*/
-    let num2 = document.getElementById("num2"); /* Me trae y almacena el valor */
-
+    let num1 = parseFloat(document.getElementById("num1").value); /* Me trae y almacena el valor*/
+    let num2 = parseFloat(document.getElementById("num2").value); /* Me trae y almacena el valor */
+    let promedio = (num1 + num2) / 2;
+    // console.log(promedio);
+    let respuesta = document.getElementById("respuesta");
+    respuesta.innerHTML = promedio;
 })
-const numero1 = document.getElementById("num1")
+suma.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value); /* Me trae y almacena el valor*/
+    let num2 = parseFloat(document.getElementById("num2").value); /* Me trae y almacena el valor */
+    let promedio = (num1 + num2);
+    // console.log(promedio);
+    let respuesta = document.getElementById("respuesta");
+    respuesta.innerHTML = promedio;
+})
+resta.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value); /* Me trae y almacena el valor*/
+    let num2 = parseFloat(document.getElementById("num2").value); /* Me trae y almacena el valor */
+    let promedio = (num1 - num2);
+    // console.log(promedio);
+    let respuesta = document.getElementById("respuesta");
+    respuesta.innerHTML = promedio;
+})
+multiplicacion.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value); /* Me trae y almacena el valor*/
+    let num2 = parseFloat(document.getElementById("num2").value); /* Me trae y almacena el valor */
+    let promedio = (num1 * num2);
+    // console.log(promedio);
+    let respuesta = document.getElementById("respuesta");
+    respuesta.innerHTML = promedio;
+})
+division.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value); /* Me trae y almacena el valor*/
+    let num2 = parseFloat(document.getElementById("num2").value); /* Me trae y almacena el valor */
+    if (num2 == 0) {
+        swal("Estás dividiendo por cero!", "¡Eres consciente de las consecuencias!");
+
+    }
+    let promedio = (num1 / num2);
+    // console.log(promedio);
+    let respuesta = document.getElementById("respuesta");
+    respuesta.innerHTML = promedio;
+})
